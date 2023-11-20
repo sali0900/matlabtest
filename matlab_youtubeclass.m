fd9c607af18790e3009a8cd823ba6a9300a52fec
sali0900 <151569501+sali0900@users.noreply.github.com>
2023-11-20 23:29:08 +0100

    Add files via upload

---
+++ matlab_youtubeclass.m
@@ -0,0 +1,14 @@
+%Example Problem
+clc, clearvars, close all
+
+%parameters
+x = linspace(0,2*pi, 10000000);
+y = sin(x);
+y_check = 0;
+
+%actions
+plot(x,y,'.'), hold on, plot([0, 2*pi],[y_check y_check], '-r')
+y_greater = y > y_check;
+
+%output
+FinalPercent = sum(y_greater) / length (y) * 100
